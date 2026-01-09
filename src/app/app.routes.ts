4fb60719e4d66cd83750fd01a18a22e81274fc1e
Cristian Figueroa <figueroaz.ca@gmail.com>
2026-01-08 20:12:06 -0500

    feat: agregar enrutamiento y componentes básicos de páginas
    Se implementó el enrutamiento para los componentes de inicio, inicio de sesión, registro y página no encontrada.
    
    Se crearon los componentes Home, Login, Register y PageNotFound con plantillas básicas.
    
    Se eliminó el archivo obsoleto app.spec.ts.
    
    Se actualizó el componente principal (app) para incluir las nuevas rutas y se corrigió la sintaxis de styleUrl.
    
    Se añadieron componentes de layout para header y footer con contenido de marcador de posición.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/pages/home/home';
+import { Login } from './features/pages/login/login';
+import { Register } from './features/pages/register/register';
+import { PageNotFound } from './features/pages/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: '404', component: PageNotFound },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: '404', pathMatch: 'full' },
+];
