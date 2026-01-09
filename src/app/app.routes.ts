040e39a590ecfe4613def6f8f7d644300f132360
Cristian Figueroa <figueroaz.ca@gmail.com>
2026-01-08 21:35:10 -0500

    feat: agregar componentes y rutas para la gestión de productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,20 @@ import { Home } from './features/pages/home/home';
 import { Login } from './features/pages/login/login';
 import { Register } from './features/pages/register/register';
 import { PageNotFound } from './features/pages/page-not-found/page-not-found';
+import { ProductEditForm } from './features/pages/products/product-edit-form/product-edit-form';
+import { ProductList } from './features/pages/products/product-list/product-list';
+import { ProductNewForm } from './features/pages/products/product-new-form/product-new-form';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: '404', component: PageNotFound },
+
+  { path: 'dashboard/products', component: ProductList },
+  { path: 'dashboard/products/new', component: ProductNewForm },
+  { path: 'dashboard/products/edit', component: ProductEditForm },
+
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: '404', pathMatch: 'full' },
 ];
