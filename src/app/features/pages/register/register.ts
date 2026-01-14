efa133ff671103a17a862943f62f71c2a5bbee4a
Cristian Figueroa <figueroaz.ca@gmail.com>
2026-01-13 21:54:52 -0500

    feat: implementar formulario de registro de usuario y eliminar formulario de usuarios nuevos

--- src/app/features/pages/register/register.ts
+++ src/app/features/pages/register/register.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  formData!: FormGroup;
 
+  constructor() {
+    this.formData = new FormGroup({
+      name: new FormControl(''),
+      username: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+      role: new FormControl(''),
+      // isActive: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formData.value);
+  }
 }
