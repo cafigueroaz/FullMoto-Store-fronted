26ac6be9306a3d1c533e4c9cb6c887033f14dcaa
Cristian Figueroa <figueroaz.ca@gmail.com>
2026-01-13 21:44:52 -0500

    feat: implementar formulario para la creación de productos

--- src/app/features/pages/products/product-new-form/product-new-form.ts
+++ src/app/features/pages/products/product-new-form/product-new-form.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-product-new-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-new-form.html',
   styleUrl: './product-new-form.css',
 })
 export class ProductNewForm {
+  formData!: FormGroup;
 
+  constructor() {
+    this.formData = new FormGroup({
+      name: new FormControl(''),
+      description: new FormControl(''),
+      price: new FormControl(0),
+      stock: new FormControl(0),
+      categoryId: new FormControl(''),
+      brand: new FormControl(''),
+      productType: new FormControl(''),
+      compatibleWith: new FormControl(''),
+      mainImage: new FormControl(''),
+      imageGallery: new FormControl(''),
+      status: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formData.value);
+  }
 }
---
+++ src/app/features/pages/users/users-new-form/users-new-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-new-form',
+  imports: [],
+  templateUrl: './users-new-form.html',
+  styleUrl: './users-new-form.css',
+})
+export class UsersNewForm {
+
+}
