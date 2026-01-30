82ab4d1bb5593e7fcf0fd31151c42121a6fd3ffa
GabrielH <gabohernandezplazas27@gmail.com>
2026-01-29 19:26:22 -0500

    feat: add category management components and routes, including list and new form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { ProductEditForm } from './features/pages/products/product-edit-form/pro
 import { ProductList } from './features/pages/products/product-list/product-list';
 import { ProductNewForm } from './features/pages/products/product-new-form/product-new-form';
 import { Card } from '../../src/app/shared/layout/card/card';
+import { CategoryList } from './features/pages/categories/category-list/category-list';
+import { CategoryNewForm } from './features/pages/categories/category-new-form/category-new-form';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
@@ -18,6 +20,9 @@ export const routes: Routes = [
   { path: 'dashboard/products/new', component: ProductNewForm },
   { path: 'dashboard/products/edit', component: ProductEditForm },
   { path: 'dashboard/products/card', component: Card },
+
+  { path: "dashboard/categories", component: CategoryList },
+  { path: "dashboard/categories/new", component: CategoryNewForm },
 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: '404', pathMatch: 'full' },
--- src/app/core/services/http-category.ts
+++ src/app/core/services/http-category.ts
@@ -6,9 +6,23 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class HttpCategory {
+private base_url: string = 'http://localhost:3000/api/v1/categories';
+
   constructor(private http: HttpClient) {}
+
+  createCategory( newCategory: any ): Observable<any> {
+    return this.http.post<any>('http://localhost:3000/api/v1/categorias/create', newCategory);
+  }
+
+  getcategoryById( ): Observable<any> {
+    return this.http.get<any>(`${this.base_url}/${this.slug}`);
+  }
 
   getAllCategories(): Observable<any> {
     return this.http.get<any>('http://localhost:3000/api/v1/categorias/get-all');
+  }
+
+  deleteCategory( id: string ){
+    return this.http.delete(`${ this.base_url}/${id}`);
   }
 }
