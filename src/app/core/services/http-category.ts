1ee83494928aead6b46e1bc105cf1fe399e05462
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-02-18 16:26:53 -0500

    [+] http-(auth,category,product): Se refactorizan los servicios

--- src/app/core/services/http-category.ts
+++ src/app/core/services/http-category.ts
@@ -6,23 +6,23 @@ import { catchError, map, Observable, of, tap } from 'rxjs';
   providedIn: 'root',
 })
 export class HttpCategory {
-private base_url: string = 'http://localhost:3000/api/v1/';
-private slug: string = 'categories';
+  private base_url: string = 'http://localhost:3000/api/v1/';
+  private slug: string = 'categories';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  createCategory( newCategory: any ): Observable<any> {
+  createCategory(newCategory: any): Observable<any> {
     return this.http.post<any>('http://localhost:3000/api/v1/categorias/create', newCategory);
   }
 
-  getcategoryById( id: string ): Observable<any> {
+  getcategoryById(id: string): Observable<any> {
     return this.http.get(`${this.base_url}/${this.slug} /${id}`);
   }
   getAllCategories(): Observable<any> {
     return this.http.get<any>('http://localhost:3000/api/v1/categorias/get-all');
   }
 
-  deleteCategory( id: string ){
-    return this.http.delete(`${ this.base_url}/${this.slug}/${id}`);
+  deleteCategory(id: string) {
+    return this.http.delete(`${this.base_url}/${this.slug}/${id}`);
   }
 }
