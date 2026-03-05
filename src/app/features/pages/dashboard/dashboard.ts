e6920e67552c1732cf6be997e58ff3e96dc0dacf
Cristian Figueroa <figueroaz.ca@gmail.com>
2026-03-05 18:11:48 -0500

    feat: refactor sidebar en dashboard para incluir gestión de usuarios y mejorar la estructura del código

--- src/app/features/pages/dashboard/dashboard.ts
+++ src/app/features/pages/dashboard/dashboard.ts
@@ -1,14 +1,19 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { HttpAuth } from '../../../core/services/http-auth';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, AsyncPipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
-  constructor(public router: Router) {}
+  constructor(
+    public router: Router,
+    public httpAuth: HttpAuth,
+  ) {}
 
   get pageTitle(): string {
     const url = this.router.url;
