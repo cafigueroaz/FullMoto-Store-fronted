c9143fd5f35a63ff3b90651a63f01a6f91c3de04
GabrielH <gabohernandezplazas27@gmail.com>
2026-03-11 18:09:31 -0500

    feat: enhance product display and styling in home page, update product fetching logic

--- src/app/core/services/http-product.ts
+++ src/app/core/services/http-product.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, tap, of} from 'rxjs';
+import { catchError, Observable, tap, of, map} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -23,13 +23,16 @@ private base_url: string = 'http://localhost:3000/api/v1/products';
     return this.http.get(`${this.base_url}/${id}`);
   }
   getAllProducts(): Observable<any[]> {
-    return this.http.get<any[]>('http://localhost:3000/api/v1/products/get-all').pipe(
-      tap((data) => {
-        console.log(data);
-      }),
-      catchError((error) => of([])),
+  return this.http
+    .get<{ products: any[] }>('http://localhost:3000/api/v1/products/get-all')
+    .pipe(
+      map(res => res.products),
+      catchError(error => {
+        console.error('Error fetching products:', error);
+        return of([]);
+      })
     );
-  }
+}
 
   deleteCategory(id: string) {
     return this.http.delete(`${this.base_url}/${this.slug}/${id}`);
