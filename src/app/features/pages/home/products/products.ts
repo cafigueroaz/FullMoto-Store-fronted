5c6a896bcd379bdd40f55e76ce713bb09252c34b
Cristian Figueroa <figueroaz.ca@gmail.com>
2026-03-10 23:28:09 -0500

    feat: refactor productos para mejorar la estructura y agregar lógica de carga dinámica

--- src/app/features/pages/home/products/products.ts
+++ src/app/features/pages/home/products/products.ts
@@ -1,10 +1,27 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HttpProduct } from '../../../../core/services/http-product';
+import { BehaviorSubject, Observable, switchMap } from 'rxjs';
+import { Card } from '../../../../shared/layout/card/card';
 
 @Component({
   selector: 'app-products',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe, Card],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
-export class Products {}
+export class Products {
+  private refreshCategoryTrigger$ = new BehaviorSubject<void>(undefined);
+  public products$: Observable<any[]>;
+
+  constructor(private httpProduct: HttpProduct) {
+    this.products$ = new Observable<any[]>();
+  }
+
+  ngOnInit() {
+    this.products$ = this.refreshCategoryTrigger$.pipe(
+      switchMap(() => this.httpProduct.getAllProducts()),
+    );
+  }
+}
